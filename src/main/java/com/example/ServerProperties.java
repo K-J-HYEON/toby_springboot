a79d19766403818af7d06dfa515383485ff2b277
KWON JAE HYEON <77037051+K-J-HYEON@users.noreply.github.com>
2024-07-15 13:05:10 +0900

    ServerProperties 수정

---
+++ src/main/java/com/example/ServerProperties.java
@@ -0,0 +1,21 @@
+package com.example;
+
+
+import com.example.config.MyAutoConfigurationProperties;
+@MyAutoConfigurationProperties(prefix = "server")
+public class ServerProperties {
+    private String contextPath;
+    private int port;
+    public String getContextPath() {
+        return contextPath;
+    }
+    public void setContextPath(String contextPath) {
+        this.contextPath = contextPath;
+    }
+    public int getPort() {
+        return port;
+    }
+    public void setPort(int port) {
+        this.port = port;
+    }
+}
